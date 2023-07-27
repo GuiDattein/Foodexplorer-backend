a73b95d656944ce0aecc3ac47619a28d98270443
guilherme <guilhermedattein@gmail.com>
2023-07-27 20:13:44 -0300

    Adicionando server

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (request, response) => {
-    response.send("Oi");
+app.post("/users", (request, response) => {
+    const {name, email, password} = request.body;
+
+    response.json({name, email, password});
 });
 
 const PORT = 3333;
