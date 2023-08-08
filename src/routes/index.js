0e5448a89de83d30f1b6373563e84b57c6356eaf
guilherme <guilhermedattein@gmail.com>
2023-08-08 20:01:01 -0300

    Finalizando C.R.U.D no backend

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const {Router} = require("express");
 
 const usersRoutes = require("./users.routes.js");
 const dishesRoutes = require("./dishes.routes.js");
+const ingredientsRoutes = require("./ingredients.routes.js")
 
 const routes = Router();
 
 routes.use("/users", usersRoutes);
 routes.use("/dishes", dishesRoutes);
+routes.use("/ingredients", ingredientsRoutes);
 
 module.exports = routes;
