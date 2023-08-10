08a1519618b378d258b864b2eadea3ac656bebc8
guilherme <guilhermedattein@gmail.com>
2023-08-09 22:24:26 -0300

    Adicionando upload de imagem em pratos

--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -1,9 +1,12 @@
-const {Router} = require("express");
+const {Router, response} = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
 const DishesController = require("../controllers/DishesController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const dishesRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 
 const dishesController = new DishesController();
 
@@ -13,5 +16,9 @@ dishesRoutes.get("/", dishesController.index);
 dishesRoutes.post("/", dishesController.create);
 dishesRoutes.get("/", dishesController.show);
 dishesRoutes.delete("/:id", dishesController.delete);
+dishesRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (req, res) => {
+    console.log(res.file.filename);
+    response.json();
+});
 
 module.exports = dishesRoutes;
